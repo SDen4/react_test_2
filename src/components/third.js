04e38adc848bedc735459d9bfc54c23cd9f8f811
SDen4 <asw325@yandex.ru>
2020-05-25 21:03:48 +0500

    component statement

--- src/components/third.js
+++ src/components/third.js
@@ -1,27 +1,38 @@
 import React, {Component} from 'react';
 
 class Third extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOpen: true
+        }
+    };
     render() {
         const {article} = this.props;
+
+        const title = this.state.isOpen && <h2>{article.title}</h2>;
+        const text = this.state.isOpen && <p>{article.text}</p>;
+        const data = this.state.isOpen && <h3>Creation article date: {article.date}</h3>;
+
         return (
             <section className='third'>
-                <h2>Third Section</h2>
-                <button onClick={handleClick}>Close</button>
-                <h2>{article.title}</h2>
-                <p>{article.text}</p>
-                {/* sample of using props */}
-                <h3> Creation article date: {article.date}</h3>
-
+                <div className="third__title">
+                    <h2>Third Section</h2>
+                    <button onClick={this.handleClick} className={this.state.isOpen ? "buttonActive" : "buttonUnActive"}>{this.state.isOpen ? 'Close' : 'Open'}</button>
+                </div>
+                {title}
+                {text}
+                {data}
                 {/* sample of using js code idside jsx (in {}) */}
                 <h3> Current date: {(new Date).toDateString()}</h3>
             </section>
         );
     };
+    handleClick = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    };
 };
-
-function handleClick() {
-    console.log('Button is working!')
-};
-
 
 export default Third;
--- src/scripts/articles.js
+++ src/scripts/articles.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: "First article",
-        text: "first article first article first article first article ",
+        text: "first article text first article text first article text  ",
         date: "01.01.01"
     },{
         title: "Second article",
-        text: "second article second article second article second article",
+        text: "second article text second article text second article text ",
         date: "02.02.02"
     },{
         title: "Third article",
-        text: "third article third article third article third article third article ",
+        text: "third article text third article text third article text ",
         date: "03.03.03"
     },{
         title: "Fourth article",
-        text: "fourth article fourth article fourth article fourth article ",
+        text: "fourth article text fourth article text fourth article text ",
         date: "04.04.04"
     }
 ]
